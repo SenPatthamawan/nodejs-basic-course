ad0bbd93bc51789cb29ba054ad076b6ea8095716
SenPatthamwan <sen.patthamwan@gmail.com>
2024-01-19 13:41:15 +0700

    Node.js CLI basic, accept command option

--- 01-node-intro/exercises/01-03-args-option/index.js
+++ 01-node-intro/exercises/01-03-args-option/index.js
@@ -1,8 +1,18 @@
 import fs from "fs";
 
 const filePath = "data.txt";
-const input = process.argv[2];
+const option = process.argv[2];
+const input = process.argv[3];
 
-fs.appendFileSync(filePath, input + "\n");
 
-console.log(input, "was written in", filePath);
+if(option === "w"){
+    console.log("can write");
+    fs.appendFileSync(filePath, input + "\n");
+    console.log(input, "was written in", filePath);
+
+}else {
+    console.error("invalid option");
+}
+
+
+
